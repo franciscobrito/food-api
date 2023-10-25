e82c5b662fa5f8e5fcee9a8a448e8feeff5bbb8c
franciscobrito <francisco.brito.ac@gmail.com>
2023-10-25 14:50:16 -0300

    Commit inicial do projeto

---
+++ food-api/src/test/java/com/facbrito/food/FoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facbrito.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
